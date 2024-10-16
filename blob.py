c9af6e4400d9c779c4502a1bcf4294b5d0cf74c4
Kreichik <130498482+Kreichik@users.noreply.github.com>
2024-10-16 12:02:03 +0500

    Update blob.py

--- blob.py
+++ blob.py
@@ -23,7 +23,7 @@ def get_csv_from_blob(blob_service_client, container_name, blob_name):
 def fetch_blob_data():
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
     now = datetime.datetime.now(datetime.timezone.utc)
-    path_to_files = f"d1test/year={now.year}/month={now.month:02}/day=16"
+    path_to_files = f"d1test/year={now.year}/month={now.month:02}/day={now.day:02}"
 
     blob_list = blob_service_client.get_container_client(container_name).list_blobs(name_starts_with=path_to_files)
     all_data = pd.DataFrame()
@@ -52,7 +52,11 @@ if not all_data.empty:
     
     # Chart 1: Time Series (Temperature or any metric over time)
     with col1:
-        fig1 = px.line(all_data, x='EventProcessedUtcTime', y='Temperature', title='Temperature Over Time')
+        fig1 = px.line(all_data, x='EventProcessedUtcTime', y='Temperature', 
+                        title='Temperature Over Time', 
+                        line_shape='linear')
+        # Update the trace color to red
+        fig1.update_traces(line=dict(color='red'))
         st.plotly_chart(fig1, use_container_width=True, key="chart1")
 
     # Chart 2: Histogram (Temperature Distribution)
@@ -62,9 +66,12 @@ if not all_data.empty:
 
     # Chart 3: Scatter plot (Temperature vs Humidity)
     with col3:
-        if 'Humidity' in all_data.columns:
-            fig3 = px.scatter(all_data, x='Temperature', y='Humidity', title='Temperature vs Humidity')
-            st.plotly_chart(fig3, use_container_width=True, key="chart3")
+        fig1 = px.line(all_data, x='EventProcessedUtcTime', y='Humidity', 
+                        title='Humidity Over Time', 
+                        line_shape='linear')
+        # Update the trace color to red
+        fig1.update_traces(line=dict(color='blue'))
+        st.plotly_chart(fig1, use_container_width=True, key="chart3")
 
     # Chart 4: Gauge (Current Temperature)
     with col4:
@@ -80,6 +87,7 @@ else:
     st.warning("No data available to display.")
 
 # Auto-refresh the page every 60 seconds
-refresh_interval = 60  # Set the refresh interval in seconds
+refresh_interval = 1  # Set the refresh interval in seconds
 time.sleep(refresh_interval)
-#st.experimental_rerun()
+st.rerun()
+
